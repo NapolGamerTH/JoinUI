4554fc33a75a735eea79d2aa4f78c197ba2c5594
Anuchit303 <anuchit303.tu@gmail.com>
2018-03-08 17:12:48 +0700

    Update JoinUI.php

--- src/Napol/JoinUI.php
+++ src/Napol/JoinUI.php
@@ -12,13 +12,21 @@ use pocketmine\utils\TextFormat;
 
 class JoinUI extends PluginBase implements Listener{
 
-    public function onEnable() : void{
-        $this->getServer()->getLogger()->info("§aJoinUI §eเปิดทำงานแล้ว!");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->configmakerthingy();
-        $this->saveDefaultConfig();
-        $this->reloadConfig();
-    }
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+     @mkdir($this->getDataFolder());  
+    $this->Config = new Config($this->getDataFolder()."license.yml", Config::YAML,[
+        "Your License Number" => "......."    
+        ]);  
+    $this->Config->save(); 
+    $this->Config->reload();                      
+        $this->getLogger()->info(TextFormat::YELLOW . "กำลังตรวจสอบข้อมูล....");            
+     if($this->Config->get("Your License Number") == "a1a2s3d4"){
+         $this->getLogger()->info(TextFormat::GREEN . "อนุญาติให้ใช้ปลั๊กอินได้");     
+    $this->Eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");  
+     }else{
+        $this->getLogger()->info(TextFormat::RED . "ไม่อนุญาติให้ใช้ปลั๊กอินได้");      
+     }   
 
     public function onDisable() : void{
         $this->getServer()->getLogger()->alert(TextFormat::RED . "§aJoinUI §cปิดทำงานแล้ว!");
